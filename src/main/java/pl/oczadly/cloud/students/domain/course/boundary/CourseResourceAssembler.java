3feda942d6b52bb7bee97edc721431623d94966c
Pawlllosss <oczadlypawe@gmail.com>
2019-12-13 20:16:01 +0100

    # added update and delete links to course controller

--- src/main/java/pl/oczadly/cloud/students/domain/course/boundary/CourseResourceAssembler.java
+++ src/main/java/pl/oczadly/cloud/students/domain/course/boundary/CourseResourceAssembler.java
@@ -1,15 +1,27 @@
 package pl.oczadly.cloud.students.domain.course.boundary;
 
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.stereotype.Component;
 import pl.oczadly.cloud.students.domain.course.entity.Course;
+import pl.oczadly.cloud.students.domain.course.entity.CourseDTO;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Component
 public class CourseResourceAssembler implements ResourceAssembler<Course, Resource<Course>> {
 
     @Override
     public Resource<Course> toResource(Course course) {
-        return new Resource<>(course);
+        Long courseId = course.getId();
+
+        Link updateLink = linkTo(methodOn(CourseRestController.class).updateCourse(courseId, new CourseDTO()))
+                .withRel("update");
+        Link deleteLink = linkTo(methodOn(CourseRestController.class).deleteCourse(courseId))
+                .withRel("delete");
+
+        return new Resource<>(course, updateLink, deleteLink);
     }
 }
