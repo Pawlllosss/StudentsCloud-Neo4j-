79f9d823e9886015aed7b0b2c78311e33007226e
Pawlllosss <oczadlypawe@gmail.com>
2019-12-13 19:22:52 +0100

    + links to update in delete in student rest controller

--- src/main/java/pl/oczadly/cloud/students/domain/student/boundary/StudentResourceAssembler.java
+++ src/main/java/pl/oczadly/cloud/students/domain/student/boundary/StudentResourceAssembler.java
@@ -1,15 +1,27 @@
 package pl.oczadly.cloud.students.domain.student.boundary;
 
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.stereotype.Component;
 import pl.oczadly.cloud.students.domain.student.entity.Student;
+import pl.oczadly.cloud.students.domain.student.entity.StudentDTO;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Component
 public class StudentResourceAssembler implements ResourceAssembler<Student, Resource<Student>> {
 
     @Override
     public Resource<Student> toResource(Student student) {
-        return new Resource<>(student);
+        Long studentId = student.getId();
+
+        Link updateLink = linkTo(methodOn(StudentRestController.class).updateStudent(studentId, new StudentDTO()))
+                .withRel("update");
+        Link deleteLink = linkTo(methodOn(StudentRestController.class).deleteStudent(studentId))
+                .withRel("delete");
+
+        return new Resource<>(student, updateLink, deleteLink);
     }
 }
